4a6714c75bb97da3297425bf433bcdf1132b6fc2
Erich Oswald <erich.oswald@ergon.ch>
2023-12-10 22:32:27 +0100

    Simplify solution for day 02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,15 +1,29 @@
+import strikt.api.*
+import strikt.assertions.*
+
+fun main() {
+    val test = listOf(
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    )
+    expectThat(part1(test)).isEqualTo(8)
+
+    val input = readInput("Day02")
+    println("part 1: ${part1(input)}")
+
+    expectThat(part2(test)).isEqualTo(2286)
+    println("part 2: ${part2(input)}")
+}
+
 const val RED_CUBES = 12
 const val GREEN_CUBES = 13
 const val BLUE_CUBES = 14
 
 data class Game(val id: Int, val selections: List<Selection>)
 data class Selection(val red: Int, val green: Int, val blue: Int)
-
-fun main() {
-    Day<Int>(2)
-        .part1(8, ::part1)
-        .part2(2286, ::part2)
-}
 
 private fun part1(input: List<String>): Int =
     input
