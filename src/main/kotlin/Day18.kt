f1c3afecaba94d35273d44e1d9f0b998f6e7d8d3
Erich Oswald <erich.oswald.privat@gmail.com>
2023-12-20 08:49:45 +0100

    Add solution for day 17 (part 2)

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -20,96 +20,124 @@ fun main() {
         "L 2 (#015232)",
         "U 2 (#7a21e3)",
     )
-    val testDigPlan = testInput.parseDigPlan()
-    val testTrenchCoords = testDigPlan.trenchCoords()
-    val testLagoon = testTrenchCoords.trench()
+    val testLagoon = testInput.parseDigPlan().trenchAreas().normalize()
     testLagoon.fillFromOutside()
-    val testVolume = testLagoon.volume()
-    expectThat(testVolume).isEqualTo(62)
+    expectThat(testLagoon.volume()).isEqualTo(62)
 
     val input = readInput("Day18")
-    val lagoon = input.parseDigPlan().trenchCoords().trench()
+    val lagoon = input.parseDigPlan().trenchAreas().normalize()
     lagoon.fillFromOutside()
     println("Part 1: ${lagoon.volume()}")
+
+    expectThat(testInput.take(1).parseCorrectedPlan()).containsExactly(DigPlanNode(R, 461937))
+    val correctedTestLagoon = testInput.parseCorrectedPlan().trenchAreas().normalize()
+    correctedTestLagoon.fillFromOutside()
+    expectThat(correctedTestLagoon.volume()).isEqualTo(952408144115)
+
+    val correctedLagoon = input.parseCorrectedPlan().trenchAreas().normalize()
+    correctedLagoon.fillFromOutside()
+    println("Part 2: ${correctedLagoon.volume()}")
 }
 
+private val ColorRegex = """.*\(#(.{5})(.)\)""".toRegex()
+
 private enum class DigDirection {
-    L, R, U, D
+    R, D, L, U
 }
 
 private enum class Terrain {
     TRENCH, OUTSIDE, INSIDE
 }
 
-private data class DigPlanNode(val direction: DigDirection, val length: Int)
-
-private data class TrenchCoord(val row: Int, val column: Int)
+private data class DigPlanNode(val direction: DigDirection, val length: Long)
+private data class Patch(val xMin: Long, val xMax: Long, val yMin: Long, val yMax: Long, var terrain: Terrain)
 
 private fun List<String>.parseDigPlan(): List<DigPlanNode> =
     map { line ->
         val parts = line.split(' ')
-        DigPlanNode(DigDirection.valueOf(parts[0]), parts[1].toInt())
+        DigPlanNode(DigDirection.valueOf(parts[0]), parts[1].toLong())
     }
 
-private fun List<DigPlanNode>.trenchCoords(): Set<TrenchCoord> {
-    val coords = mutableSetOf<TrenchCoord>()
-    var current = TrenchCoord(0, 0)
+private fun List<String>.parseCorrectedPlan(): List<DigPlanNode> =
+    map { line ->
+        val (_, distance, direction) = ColorRegex.matchEntire(line)?.groupValues ?: error("Cannot match '$line'")
+        DigPlanNode(DigDirection.entries[direction.toInt()], distance.toLong(16))
+    }
+
+private fun List<DigPlanNode>.trenchAreas(): List<Patch> {
+    val patches = mutableListOf<Patch>()
+    var x = 0L
+    var y = 0L
     forEach { node ->
-        for (move in 1..node.length) {
-            current = current.move(node.direction)
-            coords += current
+        when (node.direction) {
+            U -> {
+                patches += Patch(x, x + 1, y - node.length, y, TRENCH)
+                y -= node.length
+            }
+            D -> {
+                patches += Patch(x, x + 1, y + 1, y + node.length + 1, TRENCH)
+                y += node.length
+            }
+            L -> {
+                patches += Patch(x - node.length, x, y, y + 1, TRENCH)
+                x -= node.length
+            }
+            R -> {
+                patches += Patch(x + 1, x + 1 + node.length, y, y + 1, TRENCH)
+                x += node.length
+            }
         }
     }
-    expectThat(current).isEqualTo(TrenchCoord(0, 0))
-    return coords
-}
-
-private fun Set<TrenchCoord>.trench(): List<MutableList<Terrain>> {
-    val minRow = minOf(TrenchCoord::row)
-    val maxRow = maxOf(TrenchCoord::row)
-    val minColumn = minOf(TrenchCoord::column)
-    val maxColumn = maxOf(TrenchCoord::column)
-    return (minRow..maxRow).map { row ->
-        (minColumn..maxColumn).map { column ->
-            if (contains(TrenchCoord(row, column))) {
-                TRENCH
-            } else {
-                INSIDE
-            }
-        }.toMutableList()
+    expect {
+        that(x).isEqualTo(0)
+        that(y).isEqualTo(0)
     }
+    return patches
 }
 
-private fun List<MutableList<Terrain>>.fillFromOutside() {
+private fun List<Patch>.normalize(): List<List<Patch>> {
+    val ys = (map(Patch::yMin) + map(Patch::yMax)).sorted().distinct()
+    val xs = (map(Patch::xMin) + map(Patch::xMax)).sorted().distinct()
+    val normalizedPatches = ys.zipWithNext { y0, y1 ->
+        val row = filter { it.yMin <= y0 && y1 <= it.yMax }
+        xs.zipWithNext { x0, x1 ->
+            val column = row.filter { it.xMin <= x0 && x1 <= it.xMax }
+            if (column.isEmpty()) {
+                Patch(x0, x1, y0, y1, INSIDE)
+            } else {
+                expectThat(column).hasSize(1)
+                Patch(x0, x1, y0, y1, TRENCH)
+            }
+        }
+    }
+    return normalizedPatches
+}
+
+private fun List<List<Patch>>.fillFromOutside() {
     do {
-        var flippedCount = 0
+        var changed = false
         for (row in indices) {
             for (column in this[row].indices) {
-                val here = this[row][column]
-                if (here == INSIDE && isAnyNeighborOutside(row, column)) {
-                    this[row][column] = OUTSIDE
-                    flippedCount++
+                val area = this[row][column]
+                if (area.terrain == INSIDE && isAnyNeighborOutside(row, column)) {
+                    area.terrain = OUTSIDE
+                    changed = true
                 }
             }
         }
-    } while (flippedCount != 0)
+    } while (changed)
 }
 
-private fun List<List<Terrain>>.volume(): Int =
+private fun List<List<Patch>>.volume(): Long =
     sumOf { row ->
-        row.count { it != OUTSIDE }
+        row.filter { it.terrain != OUTSIDE }.sumOf(Patch::volume)
     }
 
-private fun List<List<Terrain>>.isAnyNeighborOutside(row: Int, column: Int): Boolean =
+private fun Patch.volume(): Long =
+    (xMax - xMin) * (yMax - yMin)
+
+private fun List<List<Patch>>.isAnyNeighborOutside(row: Int, column: Int): Boolean =
     isOutside(row -1, column) || isOutside(row + 1, column) || isOutside(row, column - 1) || isOutside(row, column + 1)
 
-private fun List<List<Terrain>>.isOutside(row: Int, column: Int): Boolean =
-    (getOrNull(row)?.getOrNull(column) ?: OUTSIDE) == OUTSIDE
-
-private fun TrenchCoord.move(direction: DigDirection): TrenchCoord =
-    when (direction) {
-        L -> copy(column = column - 1)
-        R -> copy(column = column + 1)
-        U -> copy(row = row - 1)
-        D -> copy(row = row + 1)
-    }
+private fun List<List<Patch>>.isOutside(row: Int, column: Int): Boolean =
+    (getOrNull(row)?.getOrNull(column)?.terrain ?: OUTSIDE) == OUTSIDE
