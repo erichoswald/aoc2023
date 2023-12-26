f35fbf84f48cc104346dc2e0777fee80eb2ccc47
Erich Oswald <erich.oswald.privat@gmail.com>
2023-12-26 14:51:20 +0100

    Add solution for day 23 (part 2)

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -2,10 +2,14 @@ import strikt.api.*
 import strikt.assertions.*
 
 fun main() {
-    expectThat(testIsland.longestHike(testIsland.findStartCoords(), emptySet(), 0)).isEqualTo(94)
+    expectThat(testIsland.graph().longestHike(testIsland.startCoords(), testIsland.goalCoords())).isEqualTo(94)
 
     val input = readInput("Day23")
-    println("Part 1: ${input.longestHike(input.findStartCoords(), emptySet(), 0)}")
+    println("Part 1: ${input.graph().longestHike(input.startCoords(), input.goalCoords())}")
+
+    expectThat(testIsland.withoutSlopes().graph().longestHike(testIsland.startCoords(), testIsland.goalCoords())).isEqualTo(154)
+
+    println("Part 2: ${input.withoutSlopes().graph().longestHike(input.startCoords(), input.goalCoords())}")
 }
 
 private const val FOREST = '#'
@@ -42,32 +46,71 @@ private val testIsland = listOf(
 )
 
 private typealias Island = List<String>
+private typealias IslandGraph = Map<IslandCoords, List<IslandPath>>
 
 private data class IslandCoords(val row: Int, val column: Int)
+private data class IslandPath(val from: IslandCoords, val to: IslandCoords, val length: Int)
 
-private fun Island.findStartCoords(): IslandCoords =
-    IslandCoords(0, first().indexOf("."))
+private fun Island.graph(): IslandGraph {
+    val graph = mutableMapOf<IslandCoords, MutableList<IslandPath>>()
+    addToGraph(startCoords(), graph)
+    return graph
+}
 
-private fun Island.isAtGoal(coords: IslandCoords): Boolean =
-    coords.row == lastIndex && this[coords] == PATH
+private fun Island.addToGraph(from: IslandCoords, graph: MutableMap<IslandCoords, MutableList<IslandPath>>) {
+    if (from !in graph.keys) {
+        for (to in neighbors(from)) {
+            val path = pathToNextCrossing(to, setOf(from))
+            val crossing = path.last()
+            graph.getOrPut(from, ::mutableListOf) += IslandPath(from, crossing, path.lastIndex)
+            addToGraph(crossing, graph)
+        }
+    }
+}
 
-private fun Island.longestHike(from: IslandCoords, visited: Set<IslandCoords>, length: Int): Int? =
-    if (isAtGoal(from)) {
+private tailrec fun Island.pathToNextCrossing(last: IslandCoords, path: Set<IslandCoords>): List<IslandCoords> {
+    val singleUnvisitedNeighbor = (neighbors(last) - path).singleOrNull()
+    return if (singleUnvisitedNeighbor != null) {
+        pathToNextCrossing(singleUnvisitedNeighbor, path + last)
+    } else {
+        (path + last).toList()
+    }
+}
+
+private fun IslandGraph.longestHike(from: IslandCoords, goal: IslandCoords): Int? =
+    longestHike(from, goal, emptySet(), 0)
+
+private fun IslandGraph.longestHike(from: IslandCoords, goal: IslandCoords, visited: Set<IslandCoords>, length: Int): Int? =
+    if (from == goal) {
         length
     } else {
-        when (this[from]) {
-            FOREST -> emptyList()
-            PATH -> listOfNotNull(north(from), east(from), south(from), west(from))
-            NORTH -> listOfNotNull(north(from))
-            EAST -> listOfNotNull(east(from))
-            SOUTH -> listOfNotNull(south(from))
-            WEST -> listOfNotNull(west(from))
-            else -> error("Unexpected island terrain at $from: '${this[from]}'")
-        }
-            .filterNot { it in visited }
-            .mapNotNull { to -> longestHike(to, visited + from, length + 1) }
+        val paths = this[from].orEmpty()
+        val unvisited = paths.filterNot { it.to in visited }
+        unvisited.mapNotNull { path -> longestHike(path.to, goal, visited + path.from, length + path.length) }
             .maxOrNull()
     }
+
+private fun Island.startCoords(): IslandCoords =
+    IslandCoords(0, first().indexOf(PATH))
+
+private fun Island.goalCoords(): IslandCoords =
+    IslandCoords(lastIndex, last().indexOf(PATH))
+
+private fun Island.withoutSlopes(): Island =
+    map { row ->
+        row.map { terrain -> terrain.takeIf { it == FOREST } ?: PATH }.joinToString("")
+    }
+
+private fun Island.neighbors(from: IslandCoords): List<IslandCoords> =
+    when (this[from]) {
+        FOREST -> emptyList()
+        PATH -> listOfNotNull(north(from), east(from), south(from), west(from))
+        NORTH -> listOfNotNull(north(from))
+        EAST -> listOfNotNull(east(from))
+        SOUTH -> listOfNotNull(south(from))
+        WEST -> listOfNotNull(west(from))
+        else -> error("Unexpected island terrain at $from: '${this[from]}'")
+    }.filter { this[it] != FOREST }
 
 private operator fun Island.get(coords: IslandCoords): Char =
     this[coords.row][coords.column]
